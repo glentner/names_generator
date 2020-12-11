01b2daf9df6998a33917155581e2d952ce190713
Geoffrey Lentner <glentner@purdue.edu>
2020-12-11 11:30:38 -0500

    Version 0.1.0

--- names_generator/__meta__.py
+++ names_generator/__meta__.py
@@ -11,7 +11,7 @@
 """Metadata for names_generator package."""
 
 
-__version__   = '0.0.3'
+__version__   = '0.1.0'
 __authors__   = 'Geoffrey Lentner'
 __contact__   = 'glentner@purdue.edu'
 __license__   = 'Apache License'
